a5811b4fc648768ca370fdd9620d7c4318cfa91c
javier <223222@ids.upchiapas.edu.mx>
2023-10-20 19:45:18 -0600

    finish the v1 of register and login

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
+if(process.env.NODE !== 'production'){
+    require("dotenv").config()
+} 
+
+
 const express=require("express")
-const v1UserRouter=require("./src/v1/routes/userRoutes")
 
-const cors=require("cors")
+const cors =require("cors")
+
 const app=express()
-
 const PORT=process.env.PORT||80
-app.use(cors)
-app.use(express.json())
-app.use("/api/v1",v1UserRouter)
-app.listen(PORT,()=>{
-    console.log("Hola amigos")
-})
 
+app.use(cors())
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use('/api/v1',require('./src/routes/user.route'))
+app.listen(PORT,()=>{
+    console.log(`listen ${PORT}`)
+})
 
